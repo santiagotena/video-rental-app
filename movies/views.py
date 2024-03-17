2862e47a4afc8b4c1ce09eccd48151d5dadf02b5
Santiago Tena Hernandez <stenahe@gmail.com>
2024-03-17 13:49:59 +0100

    update

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,15 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
+from .models import Movie
 
-# Create your views here.
+
 def index(request):
-	return HttpResponse("Hello World")
+    movies = Movie.objects.all()
+    return render(request, 'movies/index.html', {'movies': movies})
+
+def detail(request, movie_id):
+    try:
+        movie = Movie.objects.get(id=movie_id)
+        return render(request, 'movies/detail.html', {'movie': movie})
+    except Movie.DoesNotExist:
+        raise Http404()
